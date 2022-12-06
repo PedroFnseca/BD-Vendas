476f78a8422669547dcf53986cdc5540a4251610
pedrofnseca <pedropereira.pp264@gmail.com>
2022-12-05 22:18:46 -0300

    exercicio procedure

--- storedProcedures.sql
+++ storedProcedures.sql
@@ -99,3 +99,226 @@ create trigger trg_instCliente before insert
 		begin
 			call prc_ins_log("INS_TRIGGER", "tbl_pedido");
 		end $
+
+
+# Procedure para criação de tabela temporaria 
+delimiter $
+create procedure prc_cria_tabela_temp()
+	begin
+		create temporary table if not exists tbl_auxprod(
+			aux_prod integer not null,
+			aux_desc varchar(20),
+			qtde_ajust varchar(10),
+			data_hora datetime default now(),
+			primary key(aux_prod, data_hora)
+		)
+
+	delete from tbl_auxprod;
+end $
+
+
+# Procedure para ajuste de estoque
+delimiter $
+create procedure prc_ajusta_estoque(in qtde int, out msg char(100))
+	begin
+		-- Definição de variáveis
+		declare p_linha int default 0;
+		declare p_codigo int default 0;
+		declare p_descri varchar(100);
+		declare p_estoque int default 0;
+		declare p_status int default 0;
+
+		-- Definição do cursor
+		declare meuCursor cursor for
+			select cod_produto, nome_produto, estoque_atual from tbl_produto;
+
+		-- Definição da variavel de controle de looping do cursor
+		declare continue handler for not found set p_linha = 1;
+
+		-- Abertura do cursor
+		open meuCursor;
+
+		-- chamada da procedure para criação da tabela temporaria
+		call prc_cria_tabela_temp();
+
+		-- Looping do cursor
+		meuLoop: loop
+			fetch meuCursor into p_codigo, p_descri, p_estoque;
+
+			-- controle de existir mais registros na tabela
+			if p_linha = 1 then
+				select count(*) into p_status from tbl_auxprod;	
+			
+				if p_status > 0 then
+					-- seleciono a tabela temporaria
+					select * from tbl_auxprod;
+					leave meuLoop;
+				else
+					-- a procedure rodou mas sem nenhum processamento
+					set msg = "Nada processado";
+					select msg;
+					leave meuLoop;
+			end if;
+
+			else if p_estoque = qtde then
+				-- atualizo o estoque
+				update tbl_produto set estoque_atual = qtde + 2
+					where cod_produto = p_codigo;
+
+				-- Insere os dados na tabela temporaria
+				insert into tbl_auxprod(aux_prod, aux_desc, qtde_ajust)
+					values(p_codigo, p_descri, p_estoque + 2)
+			end if;
+
+		end loop meuLoop;
+
+		-- Fechamento do cursor
+		close meuCursor;
+end $
+
+
+# De acordo com as aulas dadas, realizem as tarefas conforme abaixo:
+# 1º - Analisem a procedure que acabamos de fazer e procure melhorar sua
+# performance, otimizando-a de acordo com a proposta passada pelo professor;
+delimiter $
+create procedure prc_cria_tabela_temp()
+	begin
+		create temporary table if not exists tbl_auxprod(
+			aux_prod integer not null,
+			aux_desc varchar(20),
+			qtde_ajust varchar(10),
+			data_hora datetime default now(),
+			primary key(aux_prod, data_hora)
+		)
+
+	delete from tbl_auxprod;
+end $
+
+
+# Procedure para ajuste de estoque
+delimiter $
+create procedure prc_ajusta_estoque(in qtde int, out msg char(100))
+	begin
+		-- Definição de variáveis
+		declare p_linha int default 0;
+		declare p_codigo int default 0;
+		declare p_descri varchar(100);
+		declare p_estoque int default 0;
+		declare p_status int default 0;
+
+		-- Definição do cursor
+	  declare meuCursor cursor for
+			select cod_produto, nome_produto, estoque_atual from tbl_produto;
+
+		-- Definição da variavel de controle de looping do cursor
+		declare continue handler for not found set p_linha = 1;
+
+		-- Verifica se a quantidade passada é maior que zero
+		if qtde <= 0 then
+			set msg = "Nada processado";
+			select msg;
+		else
+			-- Abertura do cursor
+			open meuCursor;
+
+			-- chamada da procedure para criação da tabela temporaria
+			call prc_cria_tabela_temp();
+
+			-- Looping do cursor
+			meuLoop: loop
+				fetch meuCursor into p_codigo, p_descri, p_estoque;
+
+				-- controle de existir mais registros na tabela
+				if p_linha = 1 then
+					select count(*) into p_status from tbl_auxprod;	
+				
+					if p_status > 0 then
+						-- seleciono a tabela temporaria
+						select * from tbl_auxprod;
+						leave meuLoop;
+					else
+						-- a procedure rodou mas sem nenhum processamento
+						set msg = "Nada processado";
+						select msg;
+						leave meuLoop;
+					end if;
+
+				else if p_estoque = qtde then
+					-- atualizo o estoque
+					update tbl_produto set estoque_atual = qtde + 2
+						where cod_produto = p_codigo;
+
+					-- Insere os dados na tabela temporaria
+					insert into tbl_auxprod(aux_prod, aux_desc, qtde_ajust)
+						values(p_codigo, p_descri, p_estoque + 2);
+				end if;
+				end if;
+			end loop meuLoop;
+		end if;
+
+			-- Fechamento do cursor
+			close meuCursor;
+end $
+
+
+# Modifique a procedure que usamos em cursores de forma que ao passar um
+# parâmetro (percentual de aumento), a tabela de produto tenha o valor do mesmo reajustado,
+# mas somente para os produtos em que o valor seja inferior a R$ 10,00.
+delimiter $
+create procedure prc_reajuste_percentual_prod(in percentual decimal(10, 2), out msg char(100))
+begin
+	-- Definição de variáveis
+	declare p_linha int default 0;
+	declare p_codigo int default 0;
+	declare p_descri varchar(100);
+	declare p_valor decimal(10, 2) default 0;
+	declare p_status int default 0;
+
+	-- Definição do cursor
+	declare meuCursor cursor for
+		select cod_produto, nome_produto, valor from tbl_produto;
+
+	-- Definição da variavel de controle de looping do cursor
+	declare continue handler for not found set p_linha = 1;
+
+	-- Abertura do cursor
+	open meuCursor;
+
+	-- chamada da procedure para criação da tabela temporaria
+	call prc_cria_tabela_temp();
+
+	-- Looping do cursor
+	meuLoop: loop
+		fetch meuCursor into p_codigo, p_descri, p_valor;
+
+		-- controle de existir mais registros na tabela
+		if p_linha = 1 then
+			select count(*) into p_status from tbl_auxprod;	
+			
+			if p_status > 0 then
+				-- seleciono a tabela temporaria
+				select * from tbl_auxprod;
+				leave meuLoop;
+			else
+				-- a procedure rodou mas sem nenhum processamento
+				set msg = "Nada processado";
+				select msg;
+				leave meuLoop;
+			end if;
+
+		else if p_valor < 10 then
+			-- atualizo o estoque com o percentual informado
+			update tbl_produto set valor = p_valor + (p_valor * percentual / 100)
+				where cod_produto = p_codigo;
+
+			-- Insere os dados na tabela temporaria com o percentual informado
+			insert into tbl_auxprod(aux_prod, aux_desc, qtde_ajust)
+				values(p_codigo, p_descri, p_valor + (p_valor * percentual / 100));
+		end if;
+		end if;
+
+	end loop meuLoop;
+
+	-- Fechamento do cursor
+	close meuCursor;
+end $
